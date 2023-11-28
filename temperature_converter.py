0aed6ebdbcf1e3c4a95481b7b0b710e103135e2b
mateoh2467 <144353650+mateoh2467@users.noreply.github.com>
2023-11-28 15:16:35 +0000

    making the second thing

--- temperature_converter.py
+++ temperature_converter.py
@@ -14,14 +14,29 @@ def f_to_c(temp_f):
     converted_f = (temp_f - 32) * (5/9) 
     return converted_f
 
+def k_to_f(temp_k):
+    converted_k = ((temp_k - 273.15)*(9/5)) + 32
+    return converted_k
+
+def f_to_k(temp_fa):
+    converted_f_from_k = (((temp_fa) - 32)*(5/9)) + 273.15
+    return converted_f_from_k
 #Main function that uses conditionals to decide which convert function to select 
 def main():
     if(unit == "C"):
         celsius_to_fahrenheit = c_to_f(value)
         return celsius_to_fahrenheit
     elif(unit == "F"):
+        f_unit = input("Please choose between Celsius and Kelvin: ").upper()
+        if(f_unit == "C"):
         fahrenheit_to_celsius = f_to_c(value)
         return fahrenheit_to_celsius
+        elif(f_unit =="K"):
+        fahrenheit_to_kelvin = f_to_k(value)
+        return fahrenheit_to_kelvin
+    elif(unit == "K"):
+        kelvin_to_fahrenheit = k_to_f(value)
+        return kelvin_to_fahrenheit
     else:
         warning = "Please enter C or F to specify the unit: "
         return warning 
